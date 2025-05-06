92b00938ac6759c12f9d86731bfabc8f75f09a67
louispallett <louis.pallett@outlook.com>
2025-05-06 23:43:10 +0100

    Add controller for registering new user.

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,12 +1,30 @@
 import { connectToDB } from "@/lib/db";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 import User from "@/models/User";
 
 // Register
 export async function POST(req:Request) {
     try {
+        await connectToDB();
+        const data = await req.json();
+        
+        const hashedPassword = await bcrypt.hash(data.password, 12);
 
-    } catch (err) {
+        const user = await User.create({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email.toLowerCase(),
+            password: hashedPassword
+        })
 
+        return NextResponse.json({
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email
+        }, { status: 201 });
+    } catch (err:any) {
+        console.error(err);
+        return NextResponse.json({ err }, { status: 500 })
     }
 }
