16fec3ec40875187a542ebbe5fab9ee888fdd3e0
louispallett <louis.pallett@outlook.com>
2025-05-08 09:39:40 +0100

    Add array of tag _ids property to post model.

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import { DateTime } from "luxon";
+const Schema = mongoose.Schema;
 
-const Post = new mongoose.Schema({
+const Post = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
     creationDate: { 
         type: Date, 
         required: true,
