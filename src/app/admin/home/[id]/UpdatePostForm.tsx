11cd34a41cdceb22066d2b5389a52f49eabd748a
louispallett <louis.pallett@outlook.com>
2025-11-18 15:37:39 +0000

    feat(TinyMCE Editor): Intergrate TinyEditor component with UpdatePostForm component.

--- src/app/admin/home/[id]/UpdatePostForm.tsx
+++ src/app/admin/home/[id]/UpdatePostForm.tsx
@@ -1,138 +1,162 @@
-'use client'
+"use client";
 
+// Note - it might be wise to move the forms in general into a separate file where both the create and 
+// update forms can draw from - the only difference here is the intial content, which can be an optional
+// parameter.
+
+import { PostType } from "@/lib/types";
 import axios, { AxiosResponse } from "axios";
 import { useParams } from "next/navigation";
-import { useState } from "react";
-import { useForm } from "react-hook-form"
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import TinyEditor from "../TinyEditor";
 
 type Props = {
-    title: string,
-    bannerURL?:string,
-    bannerCaption?:string,
-    synopsis:string,
-    content: string,
-    tags?: { _id: string, name: string }[]
-}
+  postData: PostType;
+};
 
-export default function UpdatePostForm({ title, bannerURL, bannerCaption, synopsis, content, tags }: Props ) {
-    const form = useForm();
-    const params = useParams();
-    const { register, control, handleSubmit, formState, watch, reset, setValue, trigger } = form;
-    const { errors } = formState;
-    const [isPending, setIsPending] = useState(false);
-    const [success, setIsSuccess] = useState(false);
-    const [serverError, setServerError] = useState(null);
+export default function UpdatePostForm({ postData }: Props) {
+  const form = useForm();
+  const params = useParams();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState,
+    watch,
+    reset,
+    setValue,
+    trigger,
+  } = form;
+  const { errors } = formState;
+  const [isPending, setIsPending] = useState(false);
+  const [success, setIsSuccess] = useState(false);
+  const [serverError, setServerError] = useState(null);
+  const editorRef = useRef<any>(null);
 
-    const onSubmit = (data:any) => {
-        setIsPending(true);
-        const selectedTags = Object.entries(data)
-            .filter(([key, value]) => tags.find(tag => tag._id === key) && value)
-            .map(([key]) => key);
+  const onSubmit = (data: any) => {
+    setIsPending(true);
+    const selectedTags = Object.entries(data)
+      .filter(
+        ([key, value]) => postData.tags.find((tag) => tag._id === key) && value,
+      )
+      .map(([key]) => key);
 
-        const formData = new FormData();
-        if (data.banner && data.banner.length > 0) {
-            formData.append('banner', data.banner[0]);
-        }
-        formData.append("title", data.title);
-        formData.append("bannerCaption", data.bannerCaption);
-        formData.append("synopsis", data.synopsis);
-        formData.append("content", data.content);
-        formData.append("tags", JSON.stringify(selectedTags));
-
-        axios.put(`/api/posts/${params.id}`, formData, {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            },
-        }).then((response:AxiosResponse) => {
-            window.location.reload();
-        }).catch((err:any) => {
-            console.log(err.message);
-            setServerError(err.message);
-            setIsPending(false);
-        });
+    if (!editorRef.current) {
+      console.log("Error with EditorRef");
+      return;
     }
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-            {(title && synopsis && content && tags) ? (
-                <>
-                    <input type="text" defaultValue={title} className="form-input text-lg font-bold" 
-                        {...register("title", {
-                            required: "Required"
-                        })}
-                    />
-                    { bannerURL && (
-                        <div className="">
-                            <p><b>Current Banner</b></p>
-                            <img src={bannerURL} alt="" />
-                        </div>
-                    )}
-                    <div className="flex flex-col md:flex-row gap-2.5 md:items-end">
-                        <div>
-                            <label htmlFor="banner" className="font-bold dark:text-slate-100">Update Banner Image</label>
-                            <div className="flex items-center gap-5">
-                                <div className="self-center">
-                                    <input
-                                        type="file"
-                                        id="banner"
-                                        accept="image/*"
-                                        className="img-upload-btn"
-                                        {...register("banner", {})}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex-1">
-                            <label htmlFor="bannerCaption" className="font-bold">Caption</label>
-                            <input
-                                type="text"
-                                className="form-input h-10"
-                                placeholder="Caption"
-                                {...register("bannerCaption", {})}
-                                defaultValue={bannerCaption}
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="synopsis" className="font-bold">Synopsis</label>
-                        <textarea id="synopsis" placeholder="Synopsis"
-                            className="form-input" {...register("synopsis", {
-                                required: "Required"
-                            })}
-                            defaultValue={synopsis}
-                        ></textarea>
-                    </div>
-                    <div>
-                        <label htmlFor="content" className="font-bold">Content</label>
-                        <textarea defaultValue={content} className="form-input" placeholder="Content"
-                            id="content"
-                            {...register("content", {
-                                required: "Required"
-                            })}
-                        ></textarea>
-                    </div>
-                </>
-            ) : (
-                <div className="flex justify-center items-center p-4">
-                    <div className="spinner h-6 w-6"></div>
-                </div>
-            )}
-            <div className="flex flex-col sm:grid grid-cols-2 gap-2.5">
-                <button className="success" type="submit">
-                    { isPending ? (
-                        <div className="spinner h-6 w-6"></div>
-                    ) : (
-                        <>Save</>
-                    )}
-                </button>
-                <button className="danger" type="button">
-                    { isPending ? (
-                        <div className="spinner h-6 w-6"></div>
-                    ) : (
-                        <>Delete</>
-                    )}
-                </button>
+    const formData = new FormData();
+    formData.append("content", editorRef.current.getContent());
+    if (data.banner && data.banner.length > 0) {
+      formData.append("banner", data.banner[0]);
+    }
+    formData.append("title", data.title);
+    formData.append("bannerCaption", data.bannerCaption);
+    formData.append("synopsis", data.synopsis);
+    formData.append("tags", JSON.stringify(selectedTags));
+
+    axios
+      .put(`/api/posts/${params.id}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response: AxiosResponse) => {
+        window.location.reload();
+      })
+      .catch((err: any) => {
+        console.log(err.message);
+        setServerError(err.message);
+        setIsPending(false);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+      {postData ? (
+        <>
+          <input
+            type="text"
+            defaultValue={postData.title}
+            className="form-input text-lg font-bold"
+            {...register("title", {
+              required: "Required",
+            })}
+          />
+          {postData.bannerURL && (
+            <div className="">
+              <p>
+                <b>Current Banner</b>
+              </p>
+              <img src={postData.bannerURL} alt="" />
             </div>
-        </form>
-    )
+          )}
+          <div className="flex flex-col md:flex-row gap-2.5 md:items-end">
+            <div>
+              <label htmlFor="banner" className="font-bold dark:text-slate-100">
+                Update Banner Image
+              </label>
+              <div className="flex items-center gap-5">
+                <div className="self-center">
+                  <input
+                    type="file"
+                    id="banner"
+                    accept="image/*"
+                    className="img-upload-btn"
+                    {...register("banner", {})}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex-1">
+              <label htmlFor="bannerCaption" className="font-bold">
+                Caption
+              </label>
+              <input
+                type="text"
+                className="form-input h-10"
+                placeholder="Caption"
+                {...register("bannerCaption", {})}
+                defaultValue={postData.bannerCaption}
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="synopsis" className="font-bold">
+              Synopsis
+            </label>
+            <textarea
+              id="synopsis"
+              placeholder="Synopsis"
+              className="form-input"
+              {...register("synopsis", {
+                required: "Required",
+              })}
+              defaultValue={postData.synopsis}
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="content" className="font-bold">
+              Content
+            </label>
+            <TinyEditor editorRef={editorRef} data={postData.content} />
+          </div>
+        </>
+      ) : (
+        <div className="flex justify-center items-center p-4">
+          <div className="spinner h-6 w-6"></div>
+        </div>
+      )}
+      <div className="flex flex-col sm:grid grid-cols-2 gap-2.5">
+        <button className="success" type="submit">
+          {isPending ? <div className="spinner h-6 w-6"></div> : <>Save</>}
+        </button>
+        <button className="danger" type="button">
+          {isPending ? <div className="spinner h-6 w-6"></div> : <>Delete</>}
+        </button>
+      </div>
+    </form>
+  );
 }
--- src/app/admin/home/[id]/page.tsx
+++ src/app/admin/home/[id]/page.tsx
@@ -1,43 +1,50 @@
-import { getPostById } from "@/lib/posts"
+import { getPostById } from "@/lib/posts";
 import UpdatePostForm from "./UpdatePostForm";
 import UpdatePublish from "./UpdatePublish";
 
-export default async function UpdatePost({ params }: { params: { id: string }}) {
-    const { id } = await params; 
-    const data = await getPostById(id);
+export default async function UpdatePost({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await params;
+  const data = await getPostById(id);
 
-    // NextJS throws warning if we don't convert _id to simple string:
-    for (let tag of data.tags) {
-        tag._id = tag._id.toString();
-    }
+  // NextJS throws warning if we don't convert _id to simple string:
+  for (let tag of data.tags) {
+    tag._id = tag._id.toString();
+  }
 
-    const fullName = data.author ? data.author.firstName + " " + data.author.lastName : "Unknown Author";
-    
-    return (
-        <div className="users-container flex flex-col gap-2.5">
-            <PostInfo published={data.published} author={fullName}/>
-            <UpdatePostForm 
-                title={data.title} 
-                bannerURL={data.bannerURL}
-                bannerCaption={data.bannerCaption}
-                synopsis={data.synopsis}
-                content={data.content}
-                tags={data.tags}
-            />
-            <UpdatePublish published={data.published} />
-        </div>
-    )
+  const fullName = data.author
+    ? data.author.firstName + " " + data.author.lastName
+    : "Unknown Author";
+
+  return (
+    <div className="users-container flex flex-col gap-2.5">
+      <PostInfo published={data.published} author={fullName} />
+      <UpdatePostForm postData={JSON.parse(JSON.stringify(data))} />
+      <UpdatePublish published={data.published} />
+    </div>
+  );
 }
 
-function PostInfo({ published, author }: { published: boolean, author: string }) {
-    return (
-        <>
-            <p className="text-right">Created by <b>{author}</b></p>
-            { published ? (
-                <div className="success self-start">Published</div>
-            ) : (
-                <div className="danger self-start">Not Published</div>
-            )}
-        </>
-    )
+function PostInfo({
+  published,
+  author,
+}: {
+  published: boolean;
+  author: string;
+}) {
+  return (
+    <>
+      <p className="text-right">
+        Created by <b>{author}</b>
+      </p>
+      {published ? (
+        <div className="success self-start">Published</div>
+      ) : (
+        <div className="danger self-start">Not Published</div>
+      )}
+    </>
+  );
 }
