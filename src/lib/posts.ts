c42e1f3fa192428a8494e235241fc2a42390d1a8
louispallett <louis.pallett@outlook.com>
2025-11-18 15:29:26 +0000

    perf(TinyMCE Editor): Add return types to getAllPosts()

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -4,50 +4,53 @@ import Post from "@/models/Post";
 import { connectToDB } from "./db";
 import { PostType } from "./types";
 
-export async function getAllPosts() {
-    try {
-        await connectToDB();
-    
-        const posts = await Post.find()
-            .populate(
-            {
-                path: "author", select: "firstName lastName -_id", model: User
-            })
-            .populate({ path: "tags", select: "name -_id", model: Tag})
-            .sort("-creationDate").lean();
-        return posts;
-    } catch (err: any) {
-        console.error("Error fetching posts:", err);
-        throw new Error(err.message || "Server failed to fetch posts");
-    }
+export async function getAllPosts(): Promise<PostType[]> {
+  try {
+    await connectToDB();
+
+    const posts = await Post.find()
+      .populate({
+        path: "author",
+        select: "firstName lastName -_id",
+        model: User,
+      })
+      .populate({ path: "tags", select: "name -_id", model: Tag })
+      .sort("-creationDate")
+      .lean();
+    return posts;
+  } catch (err: any) {
+    console.error("Error fetching posts:", err);
+    throw new Error(err.message || "Server failed to fetch posts");
+  }
 }
 
 export async function getPostById(id: string): Promise<PostType> {
-    await connectToDB();
+  await connectToDB();
 
-    const post = await Post.findById(id)
-        .populate(
-        {
-            path: "author", select: "firstName lastName -_id", model: User
-        })
-        .populate({ path: "tags", select: "name", model: Tag})
-        .lean();
-    return post;
+  const post = await Post.findById(id)
+    .populate({
+      path: "author",
+      select: "firstName lastName -_id",
+      model: User,
+    })
+    .populate({ path: "tags", select: "name", model: Tag })
+    .lean();
+  return post;
 }
 
 export async function getPostsWithTag(tagId: string): Promise<PostType[]> {
-	const posts: PostType[] = await Post.find({ tags: { $in: [tagId] } });
-	return posts;
+  const posts: PostType[] = await Post.find({ tags: { $in: [tagId] } });
+  return posts;
 }
 
 //? IMPORTANT - using .populate() with TypeScript
-// 
-// With our schemas in Mongoose, we can usually reference an ObjectId in a property like this: 
+//
+// With our schemas in Mongoose, we can usually reference an ObjectId in a property like this:
 // 	author: { type: mongoose.Types.Schema, ref: "User" }
 // This allows us to use populate() later on to ask mongoose to fetch the data stored in the user
 // with the _id stored here, so we can easily access it. This is really simple in JS and Express.
 //
-// However, when using TypeScript, the compiler demands that the type be defined. We can define a type in 
+// However, when using TypeScript, the compiler demands that the type be defined. We can define a type in
 // our types.ts file like so:
 //
 //      import { Types } from "mongoose"
@@ -74,8 +77,8 @@ export async function getPostsWithTag(tagId: string): Promise<PostType[]> {
 //
 // Right?
 //
-// Well, not quite - we face an issue here! TypeScript expects an explicit UserType (and TagType) here, but it instead gets a 
-// mongoose 'ref' to 'User', which it doesn't recognise. Fixing this is actually very simple, but it can be difficult to 
+// Well, not quite - we face an issue here! TypeScript expects an explicit UserType (and TagType) here, but it instead gets a
+// mongoose 'ref' to 'User', which it doesn't recognise. Fixing this is actually very simple, but it can be difficult to
 // find an answer - the fix is to simply define models in your populate method:
 //
 // import User from "@/models/User";
@@ -92,3 +95,4 @@ export async function getPostsWithTag(tagId: string): Promise<PostType[]> {
 // See this discussion here: https://stackoverflow.com/questions/26818071/mongoose-schema-hasnt-been-registered-for-model
 // (solution in link above by user3616725)
 //! You DO NOT need to insert the author and tag values as anything other than plain strings (representing documents _ids).
+
