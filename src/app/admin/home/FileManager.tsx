af8d6bc78f4b63f95e0ca1f0223339f36325f3de
louispallett <louis.pallett@outlook.com>
2025-11-30 21:47:19 +0000

    feat(aws-document-frontend): Import and add icons for file types in FileManager component.

--- src/app/admin/home/FileManager.tsx
+++ src/app/admin/home/FileManager.tsx
@@ -10,9 +10,11 @@ export default function FileManager({ postDocuments }: FileManagerProps) {
   return (
     <div className="standard-container file-uploader">
       <h4>File Manager</h4>
-      {postDocuments.map((file) => (
-        <FileCard file={file} url={file.url} key={file._id} />
-      ))}
+      <div className="flex flex-col my-2.5 gap-2.5">
+        {postDocuments.map((file) => (
+          <FileCard file={file} url={file.url} key={file._id} />
+        ))}
+      </div>
     </div>
   );
 }
@@ -23,13 +25,41 @@ type FileCardProps = {
 };
 
 function FileCard({ file, url }: FileCardProps) {
+  const icon = getIcon(file.mimeType);
+
   return (
-    <>
-      <p>
-        <a href={url} target="_blank">
-          {file.originalName}
-        </a>
-      </p>
-    </>
+    <div className="flex gap-2.5">
+      <a className="flex-1" href={url} target="_blank">
+        <div className="standard-container flex items-center gap-2.5">
+          <img src={`/images/file-icons/${icon}.svg`} className="h-8" />
+          <p>
+            <b>{file.originalName}</b>
+          </p>
+        </div>
+      </a>
+      <button className="danger">Delete</button>
+    </div>
   );
 }
+
+const getIcon = (mimeType: string) => {
+  switch (mimeType) {
+    case "application/pdf":
+      return "pdf";
+    case "application/msword":
+    case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+      return "word";
+    case "application/vnd.ms-excel":
+    case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+      return "excel";
+    case "application/vnd.ms-powerpoint":
+    case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+      return "powerpoint";
+    case "application/vnd.oasis.opendocument.text":
+    case "application/vnd.oasis.opendocument.spreadsheet":
+    case "application/vnd.oasis.opendocument.presentation":
+      return "text";
+    default:
+      return "default";
+  }
+};
