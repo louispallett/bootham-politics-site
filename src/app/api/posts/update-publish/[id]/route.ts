d9cab793f5e0270ac39a436cc36d8c93ae484a8c
louispallett <louis.pallett@outlook.com>
2025-12-03 14:17:37 +0000

    feat(publish backend logic): Write PUT request for updating publish status of post.

---
+++ src/app/api/posts/update-publish/[id]/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ObjectId } from "mongodb";
+import { connectToDB } from "@/lib/db";
+import HttpError from "@/lib/HttpError";
+import Post from "@/models/Post";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  try {
+    await connectToDB();
+
+    const id = await params.id;
+
+    if (!ObjectId.isValid(id)) {
+      return NextResponse.json({ error: "Invalid ID format" }, { status: 400 });
+    }
+
+    const updated = await Post.findByIdAndUpdate(
+      id,
+      [{ $set: { published: { $not: "$published" } } }],
+      { new: true }, // return the updated doc
+    );
+
+    if (!updated) {
+      throw new HttpError("Post not found", 404);
+    }
+
+    return new NextResponse(null, { status: 204 });
+  } catch (err: any) {
+    console.error(err);
+    return NextResponse.json(
+      { success: false, message: err.message || "Internal Server Error" },
+      { status: err.statusCode || 500 },
+    );
+  }
+}
