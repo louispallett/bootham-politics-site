56fdc4863050ae5be85d0daba35078f0ee3b81d3
louispallett <louis.pallett@outlook.com>
2025-12-07 21:49:50 +0000

    feat(account-settings): Add frontend Form for updating personal details.

---
+++ src/app/admin/home/account/UpdatePersonalDetailsForm.tsx
@@ -0,0 +1,162 @@
+"use client";
+
+import { UserType } from "@/lib/types";
+import axios, { AxiosError } from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type Props = {
+  details: UserType;
+};
+
+export default function UpdatePersonalDetailsForm({ details }: Props) {
+  const form = useForm();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState,
+    watch,
+    reset,
+    setValue,
+    trigger,
+  } = form;
+  const { errors } = formState;
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+  const [error, setError] = useState<AxiosError | null>(null);
+
+  const onSubmit = async (data: any) => {
+    setError(null);
+    setIsPending(true);
+    axios
+      .put(`/api/auth/${details._id}/update-details`, {
+        data,
+      })
+      .then(() => {
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+        }, 1500);
+      })
+      .catch((err: any) => {
+        console.error(err.response);
+        setError(err.response.data.message);
+      })
+      .finally(() => {
+        setIsPending(false);
+      });
+  };
+
+  return (
+    <div className="standard-container container-indigo flex flex-col gap-5">
+      <h4>Update Personal Details</h4>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        className="flex flex-col gap-2.5"
+      >
+        <div className="flex flex-col sm:grid grid-cols-2 gap-2.5">
+          <div>
+            <label htmlFor="firstName">
+              <b>First Name</b>
+            </label>
+            <input
+              type="text"
+              className="form-input"
+              defaultValue={details.firstName}
+              required
+              placeholder="First Name"
+              id="firstName"
+              {...register("firstName", {
+                required: "Required",
+                maxLength: {
+                  value: 50,
+                  message:
+                    "First name cannot be longer than a fifty (50) characters long!",
+                },
+              })}
+            />
+            <span>
+              <p className="text-red-600 font-bold mt-1.5 text-xs">
+                {errors.firstName?.message}
+              </p>
+            </span>
+          </div>
+          <div>
+            <label htmlFor="lastName">
+              <b>Last Name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="form-input"
+              defaultValue={details.lastName}
+              id="lastName"
+              required
+              {...register("lastName", {
+                required: "Required",
+                maxLength: {
+                  value: 50,
+                  message:
+                    "Last name cannot be longer than a fifty (50) characters long!",
+                },
+              })}
+            />
+            <span>
+              <p className="text-red-600 font-bold mt-1.5 text-xs text-right">
+                {errors.lastName?.message}
+              </p>
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="email">
+            <b>Email</b>
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            className="form-input"
+            defaultValue={details.email}
+            required
+            id="email"
+            {...register("email", {
+              required: "Required",
+              maxLength: {
+                value: 100,
+                message:
+                  "Email cannot be longer than a hundred (100) characters long!",
+              },
+            })}
+          />
+          <span>
+            <p className="text-red-600 font-bold mt-1.5 text-xs">
+              {errors.email?.message}
+            </p>
+          </span>
+        </div>
+        {error && (
+          <div className="standard-container bg-red-500">
+            <p>{error.message}</p>
+          </div>
+        )}
+        {isPending ? (
+          <div className="submit flex justify-center">
+            <div className="spinner h-6 w-6"></div>
+          </div>
+        ) : (
+          <>
+            {success ? (
+              <div className="success flex justify-center">
+                <p>Success!</p>
+              </div>
+            ) : (
+              <button className="submit">Save</button>
+            )}
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
--- src/app/admin/home/account/page.tsx
+++ src/app/admin/home/account/page.tsx
@@ -1,5 +1,32 @@
-export default function Account() {
-    return (
-        <></>
-    )
+import { jwtVerify } from "jose";
+import { cookies } from "next/headers";
+import { getUserById } from "@/lib/users";
+import UpdatePersonalDetailsForm from "./UpdatePersonalDetailsForm";
+
+const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET!);
+
+export default async function Account() {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("token")?.value;
+
+  const { payload } = await jwtVerify(token!, JWT_SECRET);
+  const userId: any = payload.userId;
+  const user = await getUserById(userId);
+  const userClient = {
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  };
+
+  return (
+    <div className="flex flex-col gap-2.5 sm:mx-1.5 lg:mx-5">
+      <div className="standard-container container-lime">
+        <h3 className="main-title-sm">Account Settings</h3>
+      </div>
+      <UpdatePersonalDetailsForm
+        details={JSON.parse(JSON.stringify(userClient))}
+      />
+    </div>
+  );
 }
