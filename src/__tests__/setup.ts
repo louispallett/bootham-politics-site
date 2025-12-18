ee3c5d1ece5faa5b55e82c8e727033b018ab5b90
louispallett <louis.pallett@outlook.com>
2025-12-18 19:33:33 +0000

    style(prettier): Prettier restyle of files.

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config({ path: ".env.test" });
 jest.setTimeout(10000); // (to allow for memory-server to run
 import { MongoMemoryServer } from "mongodb-memory-server";
@@ -6,26 +6,25 @@ import mongoose from "mongoose";
 
 let mongo: MongoMemoryServer;
 
-
 beforeAll(async () => {
-    mongo = await MongoMemoryServer.create();
-    const uri = mongo.getUri();
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
 
-    await mongoose.connect(uri);
+  await mongoose.connect(uri);
 });
 
 afterEach(async () => {
-    if (mongoose.connection.readyState !== 1) return;
-    const collections = await mongoose.connection.db!.collections();
-    for (let collection of collections) {
-      	await collection.deleteMany({});
-    }
-});  
+  if (mongoose.connection.readyState !== 1) return;
+  const collections = await mongoose.connection.db!.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
 
 afterAll(async () => {
-    await mongoose.connection.close();
-    if (mongo) {
-      	await mongo.stop();
-    }
+  await mongoose.connection.close();
+  if (mongo) {
+    await mongo.stop();
+  }
 });
-  
+
