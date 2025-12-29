b684121daffedeec955211315cf3837a4f8a419f
louispallett <louis.pallett@outlook.com>
2025-12-29 05:26:01 +0000

    fix(bg-img): Update bg-image position and size to adjust on smaller/larger screens. Now based on per cent on smaller screens and fixed on larger screens.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,18 +7,36 @@ import { getAllTags } from "@/lib/tags";
 import Posts from "./Posts";
 
 export default async function Home() {
-	const posts = await getAllPosts();
-	const tags = await getAllTags();
+  const posts = await getAllPosts();
+  const tags = await getAllTags();
 
-	const publishedPosts = posts.filter(post => post.published);
+  const publishedPosts = posts.filter((post) => post.published);
 
   return (
-	<div>
-		<p className="my-8">Welcome to Bootham School politics site. This site is...</p>
-		<Posts 
-			posts={JSON.parse(JSON.stringify(publishedPosts))}
-			tags={JSON.parse(JSON.stringify(tags))}
-		/>
-	</div>
-	);
+    <div>
+      <div className="users-container my-8">
+        <p>Welcome to Bootham School politics site. This site is...</p>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </p>
+      </div>
+      <Posts
+        posts={JSON.parse(JSON.stringify(publishedPosts))}
+        tags={JSON.parse(JSON.stringify(tags))}
+      />
+    </div>
+  );
 }
+
