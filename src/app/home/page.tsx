7475a9b3fa4c3282a4fc3de391f803f2c13a74e8
louispallett <louis.pallett@outlook.com>
2025-12-31 05:54:45 +0000

    chore(comments): Add comments and remove unused functions and newlines.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,21 @@
-// function sleep(ms: number) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
-
 import { getAllPosts } from "@/lib/posts";
 import { getAllTags } from "@/lib/tags";
 import Posts from "./Posts";
 
+// FIXME: Fix filter function (need to add tags into an array in order to select multiple, or make it into a drop down to select just one.)
+// TODO: Apply sort functionality
+// FIXME: General responsive design for smaller screens (down to at least 340px):
+//      - Margin (x) of containers, put these down to zero to give the articles maximum space.
+//      - Set title in HEADER to be hidden on smallest screens (but keep logo)
+//      - Fix logo on /home screen.
+//      - Wrap list of files in FileManager (wrapping file btn and delete btn with flex-col).
+//      - Tags (open client side):
+//        - Wrap these around using a grid wrap or flex wrap.
+//        - Simplify the styling on smaller screens.
+
 export default async function Home() {
   const posts = await getAllPosts();
   const tags = await getAllTags();
-
   const publishedPosts = posts.filter((post) => post.published);
 
   return (
