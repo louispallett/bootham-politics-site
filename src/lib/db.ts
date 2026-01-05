32f4bc8964e36376737814fed8f9fb222ad7f3dd
louispallett <louis.pallett@outlook.com>
2026-01-05 15:41:28 +0000

    security(types): Fix TS error on MONGODB_URI.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-export async function connectToDB() {
-  if (!process.env.MONGODB_URI) {
-    throw new Error("MONGODB_URI not set");
-  }
+const MONGODB_URI = process.env.MONGODB_URI;
 
+export async function connectToDB() {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
 
-  return mongoose.connect(process.env.MONGODB_URI);
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI not set");
+  }
+
+  return mongoose.connect(MONGODB_URI);
 }
