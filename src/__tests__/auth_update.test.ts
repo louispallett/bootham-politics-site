8a8bc9a8bd1b317823c81d76faf04bb5d68493a4
louispallett <louis.pallett@outlook.com>
2026-01-07 13:10:12 +0000

    test(user_updates): Add test for updating user details.

---
+++ src/__tests__/auth_update.test.ts
@@ -0,0 +1,53 @@
+import { POST } from "@/app/api/auth/route";
+import { PUT } from "@/app/api/auth/update/details/route";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+import { NextRequest } from "next/server";
+
+describe("Auth ID route (account alterations)", () => {
+  let token: string;
+  beforeEach(async () => {
+    await User.create({
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      password: await bcrypt.hash("HelloWorld1!", 12),
+      passkey: process.env.PASS_KEY,
+    });
+
+    const req = new Request("http:localhost/api/auth", {
+      method: "POST",
+      body: JSON.stringify({
+        email: "john.doe@example.com",
+        password: "HelloWorld1!",
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const res = await POST(req);
+    const json = await res.json();
+
+    token = json.token;
+  });
+
+  it("Changes account information", async () => {
+    const cookieHeader = `token=${token}`;
+
+    const form = new FormData();
+    form.append("firstName", "Jane");
+    form.append("lastName", "Dane");
+    form.append("email", "jane.dane@example.com");
+
+    const req = new Request("http://localhost/api/auth/update/details", {
+      method: "PUT",
+      headers: {
+        cookie: cookieHeader,
+      },
+      body: form,
+    });
+
+    const nextReq = new NextRequest(req);
+    const res = await PUT(nextReq);
+
+    expect(res.status).toBe(204);
+  });
+});
