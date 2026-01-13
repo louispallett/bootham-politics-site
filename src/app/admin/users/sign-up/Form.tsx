9119610e47259c871392cdb5b8b89371ecc06fdb
louispallett <louis.pallett@outlook.com>
2026-01-13 16:01:49 +0000

    fix(form-errors): Add form errors.

--- src/app/admin/users/sign-up/Form.tsx
+++ src/app/admin/users/sign-up/Form.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosError } from "axios";
 import { HttpError } from "@/lib/types";
 
 export default function Form() {
@@ -41,7 +41,7 @@ export default function Form() {
     setIsPending(true);
     axios
       .post(`/api/auth/register`, data)
-      .then((response: AxiosResponse) => {
+      .then(() => {
         axios
           .post("/api/auth", data)
           .then(() => {
@@ -64,71 +64,114 @@ export default function Form() {
       <h4 className="text-center">Sign Up</h4>
       <img src="/images/big-ben.svg" alt="" className="h-16" />
       <div className="form-grid">
-        <input
-          type="text"
-          className="form-input"
-          placeholder="First Name"
-          required
-          {...register("firstName", {
-            required: "Required",
-          })}
-        />
-        <input
-          type="text"
-          className="form-input"
-          placeholder="Last Name"
-          required
-          {...register("lastName", {
-            required: "Required",
-          })}
-        />
+        <div>
+          <input
+            type="text"
+            className="form-input"
+            placeholder="First Name"
+            required
+            {...register("firstName", {
+              required: "Required",
+            })}
+          />
+          {errors.firstName?.message && (
+            <p className="form-error">{String(errors.firstName?.message)}</p>
+          )}
+        </div>
+        <div>
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Last Name"
+            required
+            {...register("lastName", {
+              required: "Required",
+            })}
+          />
+          {errors.lastName?.message && (
+            <p className="form-error text-right">
+              {String(errors.lastName?.message)}
+            </p>
+          )}
+        </div>
       </div>
-      <input
-        type="email"
-        className="form-input"
-        placeholder="Email"
-        required
-        {...register("email", {
-          required: "Required",
-        })}
-      />
+      <div>
+        <input
+          type="email"
+          className="form-input"
+          placeholder="Email"
+          required
+          {...register("email", {
+            required: "Required",
+          })}
+        />
+        {errors.email?.message && (
+          <p className="form-error">{String(errors.email?.message)}</p>
+        )}
+      </div>
       <div className="form-grid">
-        <input
-          type="password"
-          className="form-input"
-          placeholder="Password"
-          required
-          {...register("password", {
-            required: "Required",
-          })}
-        />
-        <input
-          type="password"
-          className="form-input"
-          placeholder="Confirm Password"
-          required
-          {...register("confPassword", {
-            required: "Required",
-            validate: {
-              passwordMatch: (fieldValue) => {
-                return (
-                  fieldValue == watch("password") || "Passwords do not match"
-                );
+        <div>
+          <input
+            type="password"
+            className="form-input"
+            placeholder="Password"
+            required
+            {...register("password", {
+              required: "Required",
+            })}
+          />
+          {errors.password?.message && (
+            <p className="form-error">{String(errors.password?.message)}</p>
+          )}
+        </div>
+        <div>
+          <input
+            type="password"
+            className="form-input"
+            placeholder="Confirm Password"
+            required
+            {...register("confPassword", {
+              required: "Required",
+              validate: {
+                passwordMatch: (fieldValue) => {
+                  return (
+                    fieldValue == watch("password") || "Passwords do not match"
+                  );
+                },
               },
-            },
+            })}
+          />
+          {errors.confPassword?.message && (
+            <p className="form-error text-right">
+              {String(errors.confPassword?.message)}
+            </p>
+          )}
+        </div>
+      </div>
+      <div>
+        <input
+          type="password"
+          className="form-input"
+          placeholder="Pass Key"
+          required
+          {...register("passkey", {
+            required: "Required",
           })}
         />
+        {errors.passkey?.message && (
+          <p className="form-error">{String(errors.passkey?.message)}</p>
+        )}
       </div>
-      <input
-        type="password"
-        className="form-input"
-        placeholder="Pass Key"
-        required
-        {...register("passkey", {
-          required: "Required",
-        })}
-      />
-      <button className="btn submit">Submit</button>
+      {serverError && (
+        <div className="standard-container bg-red-500">
+          <p>
+            <b>Error ({serverError.status})</b>: {serverError.message}
+          </p>
+        </div>
+      )}
+      <button className="btn submit">
+        {isPending ? <div className="spinner h-6 w-6"></div> : <>Submit</>}
+      </button>
     </form>
   );
 }
