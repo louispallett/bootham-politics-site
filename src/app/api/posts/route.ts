c0d7283bd483e199482bb540e0f465360a43cf25
louispallett <louis.pallett@outlook.com>
2026-01-13 22:26:40 +0000

    security(validation): Add extra validation to file checks for banner.

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -7,8 +7,8 @@ import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 import User from "@/models/User";
 import Tag from "@/models/Tag";
-
-// const config = { api: { bodyParser: { sizeLimit: "5mb" } } };
+import HttpError from "@/lib/HttpError";
+import { fileTypeFromBuffer } from "file-type";
 
 export async function GET() {
   try {
@@ -35,6 +35,17 @@ export async function GET() {
   }
 }
 
+const MAX_FILE_SIZE = 15 * 1024 * 1024;
+
+const ALLOWED_MIME_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/tif",
+  "image/png",
+  "image/webp",
+  "image/gif",
+];
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -42,17 +53,45 @@ cloudinary.config({
 });
 
 const ValidationSchema = z.object({
-  title: z.string().trim().max(200),
-  synopsis: z.string().trim().max(1000),
-  content: z.string().min(8).max(100000),
+  title: z
+    .string()
+    .trim()
+    .min(1, { message: "Title: Required" })
+    .max(200, { message: "Title: Max length 200 characters" }),
+  synopsis: z
+    .string()
+    .trim()
+    .min(1, { message: "Synopsis: Required" })
+    .max(1000, { message: "Synopsis: Max characters 1000" }),
+  content: z
+    .string()
+    .min(7, { message: "Content: Required" })
+    .max(100000, { message: "Content: Max 100000 characters" }),
   tags: z
     .array(z.string().regex(/^[a-f\d]{24}$/i, "Invalid tag ID"))
     .optional(),
-  banner: z.instanceof(File).optional(),
-  bannerCaption: z.string().max(1000).optional(),
+  banner: z
+    .instanceof(File)
+    .refine((file) => file.size <= MAX_FILE_SIZE, {
+      message: "Banner must be smaller than 15MB",
+    })
+    .refine(
+      (file) =>
+        ALLOWED_MIME_TYPES.includes(
+          file.type,
+        ),
+      {
+        message: "Invalid banner image type",
+      },
+    )
+    .optional(),
+  bannerCaption: z
+    .string()
+    .max(1000, { message: "Banner Caption: Max 1000 characters" })
+    .optional(),
 });
 
-export async function POST(req: NextRequest | Request) {
+export async function POST(req: NextRequest) {
   try {
     await connectToDB();
 
@@ -113,17 +152,34 @@ export async function POST(req: NextRequest | Request) {
     const { title, synopsis, content, tags, banner, bannerCaption } =
       parsed.data;
 
-    // Cloudinary
+    // Cloudinary banner uploader
     let cloudinaryURL = null;
     let cloudinaryId = null;
     if (banner) {
-      const arrayBuffer = await banner.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
+      // VALIDATION CHECKS ON BANNER
+      // Check file size
+      if (banner.size > MAX_FILE_SIZE) {
+        throw new HttpError("Banner image exceeds 15MB", 400);
+      }
 
+      // Check File Type
+      // NOTE: package file-type helps prevent renamed executables (i.e. virus.jpg.exe)
+      const buffer = Buffer.from(await banner.arrayBuffer());
+      const detectedType = await fileTypeFromBuffer(buffer);
+      if (!detectedType || !ALLOWED_MIME_TYPES.includes(detectedType.mime)) {
+        throw new HttpError("Banner file type not allowed", 400);
+      }
+
+      // Upload to Cloudinary
       const result = await new Promise((resolve, reject) => {
         cloudinary.uploader
           .upload_stream(
-            { resource_type: "auto", folder: "Bootham Banners" },
+            {
+              resource_type: "image",
+              folder: "Bootham Banners",
+              max_file_size: MAX_FILE_SIZE,
+              allowed_formats: ALLOWED_MIME_TYPES,
+            },
             (error, result) => {
               if (error) reject(error);
               else resolve(result);
