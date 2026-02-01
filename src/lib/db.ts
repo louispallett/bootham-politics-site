bdc3f741e151d5eead1a1cb8e8a46fba8f65b5d6
louispallett <louis.pallett@outlook.com>
2026-02-01 20:07:37 +0000

    bug(db-caching): Fixed db.ts by declaring global types and checking mongodb connection before running.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,14 +2,40 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI not set");
+}
+
+declare global {
+  var mongoose: {
+    conn: typeof import("mongoose") | null;
+    promise: Promise<typeof import("mongoose")> | null;
+  };
+}
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
 export async function connectToDB() {
-  if (mongoose.connection.readyState >= 1) {
-    return;
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  if (!MONGODB_URI) {
-    throw new Error("MONGODB_URI not set");
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!, {
+      bufferCommands: false,
+    });
   }
 
-  return mongoose.connect(MONGODB_URI);
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
