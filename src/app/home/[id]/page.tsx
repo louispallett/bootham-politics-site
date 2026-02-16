397fface10e3cbcfc59dbf54178369183064c8b3
louispallett <louis.pallett@outlook.com>
2026-02-16 21:28:41 +0000

    fix(syntax issues): Remove git merge conflict messages.

--- src/app/home/[id]/page.tsx
+++ src/app/home/[id]/page.tsx
@@ -1,12 +1,6 @@
 import Post from "./Post";
 
-<<<<<<< HEAD
 export default async function Page({
-=======
-export const runtime = "nodejs";
-
-export default async function Post({
->>>>>>> main
   params,
 }: {
   params: Promise<{ id: string }>;
